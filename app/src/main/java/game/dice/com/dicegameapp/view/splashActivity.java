ccc4aaccdcb0680f16121c5057f6d65d9b9fce28
garcajuDev <jga.ifcd0210@gmail.com>
2018-11-26 13:08:26 +0100

    Upload2 - Logica y splashScreen

---
+++ app/src/main/java/game/dice/com/dicegameapp/view/splashActivity.java
@@ -0,0 +1,25 @@
+package game.dice.com.dicegameapp.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
